f07c652617bbdbfcec5ffde61490b958edd12f18
MoralesTomas <saquic.ttomas@gmail.com>
2026-06-07 19:53:34 -0600

    feat: previo a usar un server de multimedia

--- apps/api-gateway/src/app.ts
+++ apps/api-gateway/src/app.ts
@@ -9,6 +9,8 @@ import { profileRoutes } from "./routes/profiles.routes";
 
 import { subscriptionRoutes } from "./routes/subscriptions.routes";
 import { fxRoutes } from "./routes/fx.routes";
+import { catalogRoutes } from "./routes/catalog.routes";
+import { engagementRoutes } from "./routes/engagement.routes";
 
 export const app = express();
 
@@ -28,3 +30,6 @@ app.use("/api/profiles", profileRoutes);
 
 app.use("/api", subscriptionRoutes);
 app.use("/api", fxRoutes);
+
+app.use("/api", catalogRoutes);
+app.use("/api", engagementRoutes);
